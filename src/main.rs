402e25d46218a2bb266174a2416a707e4babf20e
AndesVL <andes.v.l@hotmail.be>
2019-02-28 01:32:44 +0100

    Fixed the launch script crashing on windows because "clear" not found

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,28 @@
-use std::process::{Command,Stdio};
 use std::env;
+use std::process::{Command, Stdio};
 
 //Launches a Racket REPL in a cleared terminal.
 //main takes two arguments: the Racket-file to run and its working directory
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let file = &args[2];
-    let dir = &args[1];
+        let args: Vec<String> = env::args().collect();
+        let file = &args[2];
+        let dir = &args[1];
 
-    //Clears the terminal
-    Command::new("clear")
-            .spawn()
-            .expect("Unable to clear terminal");
+        //Clears the terminal
+        let _clear = Command::new("clear").spawn();
 
-    //Start the REPL
-    Command::new("racket")
-            .current_dir(dir)
-            .stdin(Stdio::inherit())
-            .stdout(Stdio::inherit())
-            .stderr(Stdio::inherit())
-            .arg("-i")
-            .arg("-e")
-            .arg(format!("(require xrepl)(dynamic-enter! (symbol->string (quote {})))", file))
-            .output()
-            .expect("Unable to launch racket");
+        //Start the REPL
+        Command::new("racket")
+                .current_dir(dir)
+                .stdin(Stdio::inherit())
+                .stdout(Stdio::inherit())
+                .stderr(Stdio::inherit())
+                .arg("-i")
+                .arg("-e")
+                .arg(format!(
+                        "(require xrepl)(dynamic-enter! (symbol->string (quote {})))",
+                        file
+                ))
+                .output()
+                .expect("Unable to launch racket");
 }
